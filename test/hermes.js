e853a24d37d84497cee589c03895c3098a5ca8f2
otr4v3z <99206372+otr4v3z@users.noreply.github.com>
2022-02-16 11:48:43 -0500

    added some basic test

--- test/hermes.js
+++ test/hermes.js
@@ -1,14 +1,19 @@
 const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
+const { utils } = require('ethers');
 
 
-const PLUTUSADDRESS= "0xd32858211fcefd0be0dd3fd6d069c3e821e0aef3";
+function toWei(v) {
+    return utils.parseUnits(v, 18).toString();
+}
+
+const PLUTUSADDRESS = "0xd32858211fcefd0be0dd3fd6d069c3e821e0aef3";
 
 describe("Hermes", function () {
     let deployer, alice, bob, attacker;
     let users;
-    beforeEach(async function () {
-        console.log("before each");
+    beforeEach("Deploy contracts", async function () {
+
         [deployer, alice, bob, charlie, david, attacker] = await ethers.getSigners();
 
         const HermesFactory = await ethers.getContractFactory("Hermes", deployer);
@@ -24,21 +29,45 @@ describe("Hermes", function () {
         await plutus.connect(deployer).transfer(alice.address, "5");
 
         const myPlutusBalance = await plutus.balanceOf(deployer.address);
-        console.log("my plts", myPlutusBalance);
+
+        assert.equal((await hermes.cap()).toString(), toWei('30000000'));
     });
     describe("when user deposits", () => {
 
+        it("Should receive some Hermes token and have decreased amount of PLTS", async () => {
+            const alicePltsBalance = (await plutus.balanceOf(alice.address));
+            const aliceHrmsBalance = (await hermes.balanceOf(alice.address));
 
-        it("can deposit", async () => {
-            const alicePltsBalance = (await plutus.balanceOf(alice.address)).toString();
-            expect(alicePltsBalance).to.be.equal("5");
             await plutus.connect(alice).approve(hermes.address, alicePltsBalance);
             await hermes.connect(alice).deposit(alicePltsBalance);
-            const aliceHrmsBalance = (await hermes.balanceOf(alice.address)).toString();
-            const newAlicePltsBalance = (await plutus.balanceOf(alice.address)).toString();
 
-            expect(aliceHrmsBalance).to.be.equal("5");
-            expect(newAlicePltsBalance).to.be.equal("0");
+            const newAliceHrmsBalance = (await hermes.balanceOf(alice.address));
+            const newAlicePltsBalance = (await plutus.balanceOf(alice.address));
+
+            expect(newAliceHrmsBalance).to.be.gt(aliceHrmsBalance);
+            expect(newAlicePltsBalance).to.be.lt(alicePltsBalance);
+        });
+    });
+    describe("when cap is reached", () => {
+
+        beforeEach("Mint the max amount", async () => {
+            const maxSupply = await hermes.cap();
+            await hermes.mint(alice.address, maxSupply);
+        });
+
+        it("Should have a total supply equal to max supply", async () => {
+            const maxSupply = await hermes.cap();
+            const totalSupply = await hermes.totalSupply();
+            expect(totalSupply).to.be.equal(maxSupply);
+        });
+
+        it("Should revert on deposit due to max supply being reached.", async () => {
+            const alicePltsBalance = (await plutus.balanceOf(alice.address)).toString();
+
+            await plutus.connect(alice).approve(hermes.address, alicePltsBalance);
+            // exect revert | cap reached
+            await expect(hermes.connect(alice).deposit(alicePltsBalance)).to.be.revertedWith("ERC20Capped: cap exceeded")
+
         });
     });
 });
